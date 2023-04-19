de1418531b0e8b36909b9ccbf3aa49b81aa552c8
riparuk <faruqirifa@gmail.com>
2023-04-20 00:44:58 +0700

    add my configuration

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	/*Icon*/	/*Command*/	/*Update Interval*/	/*Update Signal*/
+	{"",		"internet",	5,	4},
+	{"",		"volume",	1,	10},
+	{"",		"fanspeed",	5,	12},
+	{"",		"brightness",	1,	11},
+	{"",		"memory",	10,	14},
+	{"",		"cpu",		10,	18},
+	{"",		"battery",	5,	3},
+	{"",		"clock",	20,	1},
+	{"",		"capslock",	1,	2},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
