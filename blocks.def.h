a273cfcaa14ebf35511552703e0ec6adc793d92d
riparuk <faruqirifa@gmail.com>
2023-04-21 20:11:46 +0700

    fix some symbol

--- blocks.def.h
+++ blocks.def.h
@@ -3,9 +3,9 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/	/*Update Interval*/	/*Update Signal*/
 	{"",		"internet",	5,	4},
 	{"",		"volume",	1,	10},
-	{"",		"fanspeed",	5,	12},
 	{"",		"brightness",	1,	11},
 	{"",		"memory",	10,	14},
+	{"",		"fanspeed",	5,	12},
 	{"",		"cpu",		10,	18},
 	{"",		"battery",	5,	3},
 	{"",		"clock",	20,	1},
@@ -13,5 +13,5 @@ static const Block blocks[] = {
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = " |";
 static unsigned int delimLen = 5;
