a7c315183610630551ab3c16a6a84e4a13c053c3
riparuk <faruqirifa@gmail.com>
2023-04-27 23:15:30 +0700

    fix not work when startup because of PATH of executed file is not setup yet

--- blocks.def.h
+++ blocks.def.h
@@ -1,15 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/	/*Update Interval*/	/*Update Signal*/
-	{"",		"internet",	5,	4},
-	{"",		"volume",	1,	10},
-	{"",		"brightness",	1,	11},
-	{"",		"memory",	10,	14},
-	{"",		"fanspeed",	5,	12},
-	{"",		"cpu",		10,	18},
-	{"",		"battery",	5,	3},
-	{"",		"clock",	20,	1},
-	{"",		"capslock",	1,	2},
+	{"",		"~/.local/bin/internet",	5,	4},
+	{"",		"~/.local/bin/volume",	1,	10},
+	{"",		"~/.local/bin/brightness",	1,	11},
+	{"",		"~/.local/bin/memory",	10,	14},
+	{"",		"~/.local/bin/fanspeed",	5,	12},
+	{"",		"~/.local/bin/cpu",		10,	18},
+	{"",		"~/.local/bin/battery",	5,	3},
+	{"",		"~/.local/bin/clock",	20,	1},
+	{"",		"~/.local/bin/capslock",	1,	2},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
