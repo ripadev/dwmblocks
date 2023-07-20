55472494a39336087760daac1708bfc50816b984
ripadev <faruqirifa@gmail.com>
2023-07-20 23:29:35 +0700

    remove some unneeded blocks

--- blocks.def.h
+++ blocks.def.h
@@ -2,14 +2,14 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/	/*Update Interval*/	/*Update Signal*/
 	{"",		"~/.local/bin/internet",	5,	4},
-	{"",		"~/.local/bin/volume",		300,	10},
-	{"",		"~/.local/bin/brightness",	300,	11},
-	{"",		"~/.local/bin/memory",		10,	14},
-	{"",		"~/.local/bin/fanspeed",	5,	12},
+	// {"",		"~/.local/bin/volume",		300,	10},
+	// {"",		"~/.local/bin/brightness",	300,	11},
+	// {"",		"~/.local/bin/memory",		10,	14},
+	// {"",		"~/.local/bin/fanspeed",	5,	12},
 	{"",		"~/.local/bin/cpu",		10,	18},
 	{"",		"~/.local/bin/battery",		5,	3},
 	{"",		"~/.local/bin/clock",		20,	1},
-	{"",		"~/.local/bin/capslock",	1,	2},
+	// {"",		"~/.local/bin/capslock",	1,	2},
 	{"",		"~/.local/bin/low-battery-notification",	120,	13},
 };
 
